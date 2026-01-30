26e4d2f29c89bd02c6cacab5eee7871e85e38da4
cuongnguyen <cuongducjerry@gmail.com>
2026-01-30 10:43:47 +0700

    Update main

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3000
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths()
+    ],
+    server: {
+      port: parseInt(env.PORT)
+    }
   }
 })
