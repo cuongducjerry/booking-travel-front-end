4b82bb02fa84fef89d3d6d55b894ab1e99bd7e9c
cuongnguyen <cuongducjerry@gmail.com>
2026-01-30 09:35:00 +0700

    Init project FE

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
